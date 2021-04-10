f471da6d57a62a1307e789601576856c902dc45c
Janis Lebron <janislebron@gmail.com>
2021-04-10 12:58:22 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -12,7 +12,7 @@ class Pokemon
     db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type )
   end
 
-  def self.find
+  def self.find(id, db)
     #find pokemon by id
     #return a new pokemon object
   end
